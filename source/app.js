9726cda24b2ffa1830ba1fd23f12b59d1a644c7a
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2016-05-01 13:59:01 -0600

    removed old call
    css stylesheet linked by editing the gulpfile.js file with a gulp task

--- source/app.js
+++ source/app.js
@@ -2,5 +2,4 @@ var React = require('react');
 var ReactDom = require('react-dom');
 var FilteredList = require('./filteredlist');
 
-//ReactDom.render(<ToDo name="hey" />, document.getElementById('app'));
 ReactDom.render(<FilteredList/>, document.getElementById('mount-point'));
